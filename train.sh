cc5b71b1d9f43bf05a52cbe5630dd328aab07b6d
linxinyang <linxinyang@meituan.com>
2021-08-25 17:27:04 +0800

    fix config

--- train.sh
+++ train.sh
@@ -27,6 +27,7 @@ python -u main_contrastive.py --configs ${CONFIGS} \
                        --gathered n \
                        --loss_balance y \
                        --log_to_file n \
+                       --save_epoch 1 \
                        --backbone ${BACKBONE} \
                        --model_name ${MODEL_NAME} \
                        --gpu 0 1 2 3 4 5 6 7 \
