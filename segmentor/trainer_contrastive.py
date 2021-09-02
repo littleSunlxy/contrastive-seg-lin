2ba0cbd66d501631a3cccbc12cb9063c6e769ea9
linxinyang <linxinyang@meituan.com>
2021-09-02 11:35:12 +0800

    fix config

--- segmentor/trainer_contrastive.py
+++ segmentor/trainer_contrastive.py
@@ -405,9 +405,9 @@ class Trainer(object):
         if self.configer.get('data', 'use_xiashi_dataset'):
             from lib.datasets.xiashi.eval import val_xiashidata
             print("evaluating on xiashi data: no split")
-            #val_xiashidata(self.seg_net, self.configer, use_split=False)
+            val_xiashidata(self.seg_net, self.configer, use_split=False)
             print("evaluating on xiashi data: splitA")
-            # val_xiashidata(self.seg_net, self.configer, use_split=True)
+            val_xiashidata(self.seg_net, self.configer, use_split=True)
         else:
             self.con_val(data_loader)
 
