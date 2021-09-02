5a231889cc570f41b05b4472f77403a243298554
linxinyang <linxinyang@meituan.com>
2021-09-02 12:45:41 +0800

    fix config

--- segmentor/trainer_contrastive.py
+++ segmentor/trainer_contrastive.py
@@ -181,11 +181,11 @@ class Trainer(object):
         self.seg_net.train()
         self.pixel_loss.train()
         start_time = time.time()
-        print("here!!!!!!!!!!")
+
         if "swa" in self.configer.get('lr', 'lr_policy'):
             normal_max_iters = int(self.configer.get('solver', 'max_iters') * 0.75)
             swa_step_max_iters = (self.configer.get('solver', 'max_iters') - normal_max_iters) // 5 + 1
-
+        print("here!!!!!!!!!!")
         if hasattr(self.train_loader.sampler, 'set_epoch'):
             self.train_loader.sampler.set_epoch(self.configer.get('epoch'))
 
