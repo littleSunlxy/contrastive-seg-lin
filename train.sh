afe09342bde0c27dcf82bb36076f59d3d0efb6ad
linxinyang <linxinyang@meituan.com>
2021-09-02 12:33:08 +0800

    fix config

--- train.sh
+++ train.sh
@@ -18,36 +18,36 @@ PRETRAINED_MODEL="${MODEL_ROOT}/hrnetv2_w48_imagenet_pretrained.pth"
 LOG_FILE="${SCRATCH_ROOT}/logs/Cityscapes/${CHECKPOINTS_NAME}.log"
 #MAX_ITERS=20000
 MAX_ITERS=20
-BATCH_SIZE=2
+BATCH_SIZE=4
 BASE_LR=0.01
 
 # --gpu 0 1 2 3 4 5 6 7 \
 
-#python -u main_contrastive.py --configs ${CONFIGS} \
-#                       --drop_last y \
-#                       --phase train \
-#                       --gathered n \
-#                       --loss_balance y \
-#                       --log_to_file n \
-#                       --backbone ${BACKBONE} \
-#                       --model_name ${MODEL_NAME} \
-#                       --gpu 4 \
-#                       --use_xiashi_dataset \
-#                       --data_dir ${DATA_DIR} \
-#                       --loss_type ${LOSS_TYPE} \
-#                       --max_iters ${MAX_ITERS} \
-#                       --checkpoints_root ${CHECKPOINTS_ROOT} \
-#                       --checkpoints_name ${CHECKPOINTS_NAME} \
-#                       --pretrained ${PRETRAINED_MODEL} \
-#                       --train_batch_size ${BATCH_SIZE} \
-#                       --distributed \
-#                       --base_lr ${BASE_LR} \
-#                       2>&1 | tee ${LOG_FILE}
+python -u main_contrastive.py --configs ${CONFIGS} \
+                       --drop_last y \
+                       --phase train \
+                       --gathered n \
+                       --loss_balance y \
+                       --log_to_file n \
+                       --backbone ${BACKBONE} \
+                       --model_name ${MODEL_NAME} \
+                       --gpu 4 5 6 7 \
+                       --use_xiashi_dataset \
+                       --data_dir ${DATA_DIR} \
+                       --loss_type ${LOSS_TYPE} \
+                       --max_iters ${MAX_ITERS} \
+                       --checkpoints_root ${CHECKPOINTS_ROOT} \
+                       --checkpoints_name ${CHECKPOINTS_NAME} \
+                       --pretrained ${PRETRAINED_MODEL} \
+                       --train_batch_size ${BATCH_SIZE} \
+                       --distributed \
+                       --base_lr ${BASE_LR} \
+                       2>&1 | tee ${LOG_FILE}
 
 
 export CFG_1="configs/meituan_xiashi/mergev1.4_val_nosplit.yaml"
 export CFG_2="configs/meituan_xiashi/mergev1.4_val_splitA.yaml"
-export GPUS=4
+export GPUS=4,5,6,7
 echo "-----------------------------------------------"
 echo "Start to valid no-split experiment"
 echo "-----------------------------------------------"
