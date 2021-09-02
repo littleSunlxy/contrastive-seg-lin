0ebf51502a6c65524161198b03f22dda99d833dd
linxinyang <linxinyang@meituan.com>
2021-09-02 16:49:00 +0800

    fix config

--- train.sh
+++ train.sh
@@ -18,7 +18,7 @@ PRETRAINED_MODEL="${MODEL_ROOT}/hrnetv2_w48_imagenet_pretrained.pth"
 LOG_FILE="${SCRATCH_ROOT}/logs/Cityscapes/${CHECKPOINTS_NAME}.log"
 #MAX_ITERS=20000
 MAX_ITERS=40000
-BATCH_SIZE=1
+BATCH_SIZE=8
 BASE_LR=0.01
 
 # --gpu 0 1 2 3 4 5 6 7 \
@@ -31,7 +31,7 @@ python -u main_contrastive.py --configs ${CONFIGS} \
                        --log_to_file n \
                        --backbone ${BACKBONE} \
                        --model_name ${MODEL_NAME} \
-                       --gpu 4 \
+                       --gpu 4 5 6 7 \
                        --use_xiashi_dataset \
                        --data_dir ${DATA_DIR} \
                        --loss_type ${LOSS_TYPE} \
